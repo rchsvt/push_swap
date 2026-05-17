e0c32ac3e5053cf5c2116dbc655ab5f851e35305
remy chavastel <remy_chvst@MacBook-Pro-de-remy.local>
2026-05-17 12:07:35 +0200

    Fix small sort and import working project

--- main.c
+++ main.c
@@ -12,40 +12,66 @@
 
 #include "push_swap.h"
 
+static int	get_stack_size(t_stack *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
 static void	sort_mode(t_stack **a, t_stack **b,
 	double disorder, char *mode)
 {
-	if (!ft_strncmp(mode, "--simple", 9))
+	int	size;
+
+	size = get_stack_size(*a);
+	if (size <= 5)
+		sort_five(a, b);
+	else if (!ft_strncmp(mode, "--simple", 9))
 		sort_five(a, b);
 	else if (!ft_strncmp(mode, "--medium", 9))
 		chunk_sort(a, b);
 	else if (!ft_strncmp(mode, "--complex", 10))
 		radix_sort(a, b);
-	else if (disorder < 0.2)
-		sort_five(a, b);
 	else if (disorder < 0.5)
 		chunk_sort(a, b);
 	else
 		radix_sort(a, b);
 }
 
-static char	*get_mode(char **argv, int *start)
+static int	is_mode(char *arg)
+{
+	if (!arg)
+		return (0);
+	if (!ft_strncmp(arg, "--simple", 9))
+		return (1);
+	if (!ft_strncmp(arg, "--medium", 9))
+		return (1);
+	if (!ft_strncmp(arg, "--complex", 10))
+		return (1);
+	if (!ft_strncmp(arg, "--adaptive", 11))
+		return (1);
+	return (0);
+}
+
+static char	*get_mode(int argc, char **argv, int *start)
 {
 	char	*mode;
 
 	mode = "--adaptive";
-	if (!ft_strncmp(argv[*start], "--bench", 8))
+	if (*start < argc && !ft_strncmp(argv[*start], "--bench", 8))
 		(*start)++;
-	if (!ft_strncmp(argv[*start], "--simple", 9))
-		mode = "--simple";
-	else if (!ft_strncmp(argv[*start], "--medium", 9))
-		mode = "--medium";
-	else if (!ft_strncmp(argv[*start], "--complex", 10))
-		mode = "--complex";
-	else if (!ft_strncmp(argv[*start], "--adaptive", 11))
-		mode = "--adaptive";
-	if (argv[*start][0] == '-')
+	if (*start < argc && is_mode(argv[*start]))
+	{
+		mode = argv[*start];
 		(*start)++;
+	}
 	return (mode);
 }
 
@@ -62,7 +88,9 @@ int	main(int argc, char **argv)
 	a = NULL;
 	b = NULL;
 	start = 1;
-	mode = get_mode(argv, &start);
+	mode = get_mode(argc, argv, &start);
+	if (start >= argc)
+		return (0);
 	init_stack(&a, argv + start - 1);
 	if (is_sorted(a))
 		return (free_stack(&a), 0);
